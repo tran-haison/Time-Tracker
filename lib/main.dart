6d18b049637005cbe6cd3a3a9089f05c87a65049
tran-haison <haison250199@gmail.com>
2021-06-26 22:42:08 +0700

    Add dcdg dependancy to generate uml diagram

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_flutter_course/app/landing_page.dart';
 import 'package:time_tracker_flutter_course/services/auth.dart';
+import 'package:dcdg/dcdg.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
